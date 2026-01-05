161371c304bfd3f51d8d433da868dea3a6c0b69f
rorrxr <rorrxr@naver.com>
2026-01-05 21:20:52 +0900

    chore : Spring 프로젝트 초기 설정

---
+++ src/main/java/org/swyp/linkit/LinkItApplication.java
@@ -0,0 +1,13 @@
+package org.swyp.linkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/swyp/linkit/LinkItApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swyp.linkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
