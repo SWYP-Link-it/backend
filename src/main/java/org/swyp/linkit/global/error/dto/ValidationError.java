0642245e45bfdf1b7d10a99e46b33963e98d55c7
홍정화 <ekdan31@gmail.com>
2026-01-15 14:18:24 +0900

    feat : ValidationError 작성

---
+++ src/main/java/org/swyp/linkit/global/error/dto/ValidationError.java
@@ -0,0 +1,14 @@
+package org.swyp.linkit.global.error.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ValidationError {
+
+    private final String field;
+    private final String message;
+    private final String rejectedValue;
+
+}
