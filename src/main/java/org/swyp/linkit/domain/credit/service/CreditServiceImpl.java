5647660b15c693d516baca0e1d3b2d46de6d7497
홍정화 <ekdan31@gmail.com>
2026-01-19 20:28:52 +0900

    feat: Credit Service 작성 및 Credit 생성, 리워드 지급 구현

---
+++ src/main/java/org/swyp/linkit/domain/credit/service/CreditServiceImpl.java
@@ -0,0 +1,57 @@
+package org.swyp.linkit.domain.credit.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.swyp.linkit.domain.credit.dto.CreditDto;
+import org.swyp.linkit.domain.credit.entity.Credit;
+import org.swyp.linkit.domain.credit.entity.HistoryType;
+import org.swyp.linkit.domain.credit.repository.CreditRepository;
+import org.swyp.linkit.domain.user.entity.User;
+import org.swyp.linkit.global.error.exception.NotFoundCreditException;
+
+@Service
+@RequiredArgsConstructor
+public class CreditServiceImpl implements CreditService{
+
+    private static final int SIGNUP_REWARD = 2;
+    private static final int PROFILE_REWARD = 1;
+
+    private final CreditRepository creditRepository;
+    private final CreditHistoryService historyService;
+
+    @Transactional
+    @Override
+    public CreditDto createCredit(User user) {
+        // credit 생성, save
+        Credit credit = creditRepository.save(Credit.create(user, 0));
+        return CreditDto.from(credit);
+    }
+
+    @Transactional
+    @Override
+    public CreditDto rewardCreditOnSignupSetup(User user) {
+        return applyReward(user, SIGNUP_REWARD, HistoryType.SIGNUP_REWARD);
+    }
+
+    @Transactional
+    @Override
+    public CreditDto rewardCreditOnProfileSetup(User user) {
+        return applyReward(user, PROFILE_REWARD, HistoryType.PROFILE_REWARD);
+    }
+
+    private CreditDto applyReward(User user, int amount, HistoryType type) {
+        // 1. credit 조회
+        Credit credit = getCreditByUserId(user.getId());
+        // 2. credit 지급
+        credit.increaseAmount(amount);
+        // 3. creditHistory 생성
+        historyService.createCreditHistory(user, amount, credit.getAmount(), type);
+        return CreditDto.from(credit);
+    }
+
+    private Credit getCreditByUserId(Long userId){
+        return creditRepository.findByUserId(userId)
+                .orElseThrow(NotFoundCreditException::new);
+    }
+}
