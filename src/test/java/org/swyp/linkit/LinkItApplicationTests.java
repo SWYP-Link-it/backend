a5e703fdbc33c0df55d4bd958d089535f967505d
rorrxr <rorrxr@naver.com>
2026-01-22 00:27:43 +0900

    chore : test쪽 불필요 import 제거

--- src/test/java/org/swyp/linkit/LinkItApplicationTests.java
+++ src/test/java/org/swyp/linkit/LinkItApplicationTests.java
@@ -2,10 +2,7 @@ package org.swyp.linkit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
