18667e7d425cf0bc90bc5ecc15fa9b82facd532c
홍정화 <ekdan31@gmail.com>
2026-01-24 01:23:42 +0900

    refactor: CreditHistory Entity 구체화
    기존 임시 로직 정리

--- src/main/java/org/swyp/linkit/domain/credit/entity/CreditHistory.java
+++ src/main/java/org/swyp/linkit/domain/credit/entity/CreditHistory.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.swyp.linkit.domain.exchange.entity.SkillExchange;
 import org.swyp.linkit.domain.user.entity.User;
 import org.swyp.linkit.global.common.domain.BaseTimeEntity;
 
@@ -18,66 +19,112 @@ public class CreditHistory extends BaseTimeEntity {
     @Column(name = "credit_history_id")
     private Long id;
 
+    // 내역 주인
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    /**
+     * 상대방 (리워드 지급 시 null)
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_user_id", nullable = true)
+    private User targetUser;
+
+    /**
+     * 스킬 교환 (리워드 지급 시 null)
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_exchange_id", nullable = true)
+    private SkillExchange skillExchange;
+
+    /**
+     * 내역 명 (스킬명 혹은 리워드명)
+     */
+    @Column(nullable = false)
+    private String contentName;
+
+    // ADD, USE
+    @Enumerated(EnumType.STRING)
+    private SupplyType supplyType;
+
+    // 변동 크레딧 (+ / -)
     @Column(nullable = false)
     private int changeAmount;
 
+    // 거래 후 잔액
     @Column(nullable = false)
     private int balanceAfter;
 
+    // 거래 타입
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private HistoryType historyType;
 
-    // 크레딧 변동이 스킬 교환일 경우만 세팅
-    private Long skillExchangeId;
-
-    // 크레딧 변동이 스킬 교환일 경우만 세팅
-    private Long targetUserId;
-
     @Builder(access = AccessLevel.PRIVATE)
-    private CreditHistory(int changeAmount, int balanceAfter, HistoryType historyType,
-                          Long skillExchangeId, Long targetUserId) {
+    public CreditHistory(SupplyType supplyType, String contentName, int changeAmount,
+                         int balanceAfter, HistoryType historyType) {
+        this.supplyType = supplyType;
+        this.contentName = contentName;
         this.changeAmount = changeAmount;
         this.balanceAfter = balanceAfter;
         this.historyType = historyType;
-        this.skillExchangeId = skillExchangeId;
-        this.targetUserId = targetUserId;
     }
 
     // == 생성 메서드 ==
-    public static CreditHistory createReward(User user, int changeAmount,
-                                             int balanceAfter, HistoryType historyType) {
+
+    /**
+     *  리워드 생성 내역
+     */
+    public static CreditHistory createReward(User user, int amount, int balanceAfter, HistoryType historyType) {
         CreditHistory creditHistory = CreditHistory.builder()
-                .changeAmount(changeAmount)
+                .supplyType(SupplyType.ADD)
+                .contentName(historyType.getContentName())
+                .changeAmount(amount)
                 .balanceAfter(balanceAfter)
                 .historyType(historyType)
                 .build();
+
         // User 연관관계 주입
         creditHistory.assignUser(user);
         return creditHistory;
     }
 
-    public static CreditHistory createSkillExchange(User user, int changeAmount, int balanceAfter,
-                                                    HistoryType historyType, Long skillExchangeId,
-                                                    Long targetUserId) {
+    /**
+     *  스킬 교환으로 인한 내역 생성
+     */
+    public static CreditHistory createSkillExchange(User user, User targetUser, SkillExchange skillExchange,
+                                                    SupplyType supplyType, int amount, int balanceAfter,
+                                                    HistoryType historyType) {
         CreditHistory creditHistory = CreditHistory.builder()
-                .changeAmount(changeAmount)
+                .supplyType(supplyType)
+                .contentName(skillExchange.getSkillName())
+                .changeAmount(amount)
                 .balanceAfter(balanceAfter)
                 .historyType(historyType)
-                .skillExchangeId(skillExchangeId)
-                .targetUserId(targetUserId)
                 .build();
         // User 연관관계 주입
         creditHistory.assignUser(user);
+        // targetUser 연관관계 주입
+        creditHistory.assignTargetUser(targetUser);
+        // SkillExchange 연관관계 주입
+        creditHistory.assignSkillExchange(skillExchange);
+
         return creditHistory;
     }
 
     // == User와 연관관계 설정 ==
     private void assignUser(User user) {
         this.user = user;
+    }
+
+    // == targetUser와 연관관계 설정 ==
+    private void assignTargetUser(User targetUser) {
+        this.targetUser = targetUser;
+    }
+
+    // == SkillExchange와 연관관계 설정 ==
+    private void assignSkillExchange(SkillExchange skillExchange) {
+        this.skillExchange = skillExchange;
     }
 }
--- src/main/java/org/swyp/linkit/domain/credit/entity/HistoryType.java
+++ src/main/java/org/swyp/linkit/domain/credit/entity/HistoryType.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum HistoryType {
-    SIGNUP_REWARD("회원가입 리워드"),
-    PROFILE_REWARD("프로필 작성 리워드"),
-    EVENT_REWARD("이벤트 리워드"),
-    EXCHANGE_REQUEST("스킬 교환 요청"),
-    EXCHANGE_REJECTED("스킬 교환 요청 거절"),
-    EXCHANGE_EXPIRED("스킬 교환 요청 만료"),
-    EXCHANGE_SETTLED("스킬 교환 요청 정산");
+    SIGNUP_REWARD("회원가입","리워드"),
+    PROFILE_REWARD("프로필 작성", "리워드"),
+    EXCHANGE_REQUEST("스킬 교환", "요청"),
+    EXCHANGE_REJECTED("스킬 교환","거절"),
+    EXCHANGE_EXPIRED("스킬 교환","만료"),
+    EXCHANGE_CANCELED("스킬 교환","취소"),
+    EXCHANGE_SETTLED("스킬 교환","정산");
 
-    private final String description;
+    private final String contentName;
+    private final String statusLabel;
 }
