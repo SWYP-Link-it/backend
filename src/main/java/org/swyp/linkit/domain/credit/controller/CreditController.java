e77831df766674b2e31488ab5abb9c308a78e266
홍정화 <ekdan31@gmail.com>
2026-01-23 18:49:10 +0900

    feat: Credit 잔액 조회 및 User 정보 조회  (#20)
    * feat: Credit 잔액 조회 및 유저 정보 조회 구현
    
    * feat: Credit 잔액 조회 및 유저 정보 조회 구현
    
    Dto 누락
    
    * refactor: CreditBalanceResponseDto 변수명 수정
    
    * refactor: CreditController url 수정
    
    getCreditBalanceWithUserDetails url 수정
    
    * refactor: CreditController url 수정
    
    getCreditBalanceWithUserDetails url 수정

--- src/main/java/org/swyp/linkit/domain/credit/controller/CreditController.java
+++ src/main/java/org/swyp/linkit/domain/credit/controller/CreditController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.swyp.linkit.domain.credit.dto.CreditDto;
+import org.swyp.linkit.domain.credit.dto.CreditWithUserDetailsDto;
 import org.swyp.linkit.domain.credit.dto.response.CreditBalanceResponseDto;
+import org.swyp.linkit.domain.credit.dto.response.CreditBalanceWithUserDetailsResponseDto;
 import org.swyp.linkit.domain.credit.service.CreditService;
 import org.swyp.linkit.global.auth.oauth.CustomOAuth2User;
 import org.swyp.linkit.global.common.dto.ApiResponseDto;
@@ -25,28 +27,39 @@ public class CreditController {
 
     private final CreditService creditService;
 
-
+    /**
+     *  크레딧 잔액 조회
+     */
     @Operation(summary = "사용자의 크레딧 잔액 조회", description = "사용자의 크레딧 잔액만을 조회합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "크레딧 잔액 조회 성공", useReturnTypeSchema = true),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청 파라미터"),
+            @ApiResponse(responseCode = "200", description = "조회 성공", useReturnTypeSchema = true),
             @ApiResponse(responseCode = "401", description = "인증이 필요합니다."),
             @ApiResponse(responseCode = "404", description = "크레딧 정보를 찾을 수 없습니다.")})
     @GetMapping(value = "/balance", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ApiResponseDto<CreditBalanceResponseDto>> getCreditBalance(
-            @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+    public ResponseEntity<ApiResponseDto<CreditBalanceResponseDto>> getCreditBalance(@AuthenticationPrincipal CustomOAuth2User oAuth2User) {
 
         CreditDto creditDto = creditService.getCreditBalance(oAuth2User.getUserId());
         CreditBalanceResponseDto responseDto = CreditBalanceResponseDto.from(creditDto);
 
-        return ResponseEntity.ok().body(ApiResponseDto.success("크레딧 잔액 조회 성공", responseDto));
+        return ResponseEntity.ok().body(ApiResponseDto.success("조회 성공", responseDto));
     }
 
-    // todo
-//    @Operation(
-//            summary = "사용자의 크레딧 잔액 및 유저 정보 조회",
-//            description = "우저의 크레딧 잔액 및 프로필 이미지, 닉네임을 조회합니다."
-//    )
-//    // 크레딧 사용 내역에서 간단한 사용자 정보 + 크레딧
-//    @GetMapping("/summary")
+    /**
+     *  크레딧 잔액 및 유저 정보 조회
+     */
+    @Operation(summary = "사용자의 크레딧 잔액 및 유저 정보 조회", description = "사용자의 크레딧 잔액과 유저의 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "401", description = "인증이 필요합니다."),
+            @ApiResponse(responseCode = "404", description = "크레딧 정보를 찾을 수 없습니다.")})
+    @GetMapping(value = "/balance-user-details", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponseDto<CreditBalanceWithUserDetailsResponseDto>> getCreditBalanceWithUserDetails(
+            @AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+
+        CreditWithUserDetailsDto cd = creditService.getCreditBalanceWithUserDetails(oAuth2User.getUserId());
+        CreditBalanceWithUserDetailsResponseDto responseDto = CreditBalanceWithUserDetailsResponseDto.from(cd);
+
+        return ResponseEntity.ok().body(ApiResponseDto.success("조회 성공", responseDto));
+    }
+
 }
