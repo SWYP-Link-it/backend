ebfdf32570e334ac54ec69a7e294d076345212a0
홍정화 <ekdan31@gmail.com>
2026-01-25 18:42:18 +0900

    fix: ExceptionGlobalHandler import 수정
    MethodArgumentTypeMismatchException -> import 수정

--- src/main/java/org/swyp/linkit/global/handler/GlobalExceptionHandler.java
+++ src/main/java/org/swyp/linkit/global/handler/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package org.swyp.linkit.global.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentTypeMismatchException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
