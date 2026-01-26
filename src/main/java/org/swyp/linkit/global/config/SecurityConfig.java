0897f5b30b7f44ede2417a2b3a4e0155f07514b2
rorrxr <rorrxr@naver.com>
2026-01-27 02:05:39 +0900

    fix : SecurityConfig HTTPMethod 권한 수정

--- src/main/java/org/swyp/linkit/global/config/SecurityConfig.java
+++ src/main/java/org/swyp/linkit/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.swyp.linkit.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -46,6 +47,7 @@ public class SecurityConfig {
 
                 // 요청 권한 설정
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/",
                                 "/error",
