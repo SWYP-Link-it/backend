906708a987d9de1e023513a79b093fc0abc195a7
홍정화 <ekdan31@gmail.com>
2026-01-27 13:42:34 +0900

    test: TestRedisConfig 작성 및 ApplicationTests 의 Redis Mock 제거
    @SpringBootTest 수행 시 @ActiveProfile("test") @Import(TestRedisConfig) 사용

--- src/test/java/org/swyp/linkit/LinkItApplicationTests.java
+++ src/test/java/org/swyp/linkit/LinkItApplicationTests.java
@@ -2,22 +2,9 @@ package org.swyp.linkit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class LinkItApplicationTests {
-
-    @MockitoBean
-    private RedisConnectionFactory redisConnectionFactory;
-
-    @MockitoBean
-    private StringRedisTemplate stringRedisTemplate;
-
-    @MockitoBean
-    private RedisMessageListenerContainer redisMessageListenerContainer;
 
     @Test
     void contextLoads() {
