139fb403ac81a2e10fb6e57f88dddca7acc0daa5
홍정화 <ekdan31@gmail.com>
2026-01-27 14:21:32 +0900

    test: ApplicationTest TestRedisConfig 적용

--- src/test/java/org/swyp/linkit/LinkItApplicationTests.java
+++ src/test/java/org/swyp/linkit/LinkItApplicationTests.java
@@ -2,7 +2,11 @@ package org.swyp.linkit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@Import(TestRedisConfig.class)
+@ActiveProfiles("test")
 @SpringBootTest
 class LinkItApplicationTests {
 
