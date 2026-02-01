597ca246f56364ebb47785b0740f61b2e6888f93
홍정화 <ekdan31@gmail.com>
2026-02-01 19:28:51 +0900

    refactor: SettlementRepository의 findSettleTargets 메서드 수정
    SkillExchange, Receiver Fetch Join ->  SkillExchange, Requester, Receiver, ReceiverSkill, ReceiverProfile Fetch Join

--- src/main/java/org/swyp/linkit/domain/settlement/repository/SettlementRepository.java
+++ src/main/java/org/swyp/linkit/domain/settlement/repository/SettlementRepository.java
@@ -3,8 +3,8 @@ package org.swyp.linkit.domain.settlement.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.swyp.linkit.domain.settlement.entitiy.Settlement;
-import org.swyp.linkit.domain.settlement.entitiy.SettlementStatus;
+import org.swyp.linkit.domain.settlement.entity.Settlement;
+import org.swyp.linkit.domain.settlement.entity.SettlementStatus;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,11 +17,14 @@ public interface SettlementRepository extends JpaRepository<Settlement, Long> {
 
     /**
      *  정산 처리 대상 조회
-     *  SkillExchange, Receiver Fetch Join
+     *  SkillExchange, Requester, Receiver, ReceiverSkill, ReceiverProfile Fetch Join
      */
     @Query("SELECT s FROM Settlement s " +
             "JOIN FETCH s.skillExchange se " +
-            "JOIN FETCH s.receiver re " +
+            "JOIN FETCH s.receiver " +
+            "JOIN FETCH se.requester " +
+            "JOIN FETCH se.receiverSkill rs " +
+            "JOIN FETCH rs.userProfile " +
             "WHERE s.status = :pending " +
             "AND (se.scheduledDate < :today OR (se.scheduledDate = :today AND se.endTime <= :nowTime))")
     List<Settlement> findSettleTargets(@Param("pending") SettlementStatus pending,
