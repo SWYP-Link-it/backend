a7e7a1fa2a0ed9f9003832d624ccde6a2245e347
홍정화 <ekdan31@gmail.com>
2026-02-02 23:14:12 +0900

    refactor: SchedulerConfig -> 테스트 환경을 위한 @ConditionalOnProperty 설정 추가

--- src/main/java/org/swyp/linkit/global/config/SchedulerConfig.java
+++ src/main/java/org/swyp/linkit/global/config/SchedulerConfig.java
@@ -1,9 +1,15 @@
 package org.swyp.linkit.global.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(
+        name = "scheduler.enabled",
+        havingValue = "true",
+        matchIfMissing = true
+)
 public class SchedulerConfig {
 }
