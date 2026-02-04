f08b7769980c451166cf6faa17be624a7d883879
jaey-oung <jaeywodud7@gmail.com>
2026-02-05 02:51:08 +0900

    feat: 닉네임 변경 서비스 구현

--- src/main/java/org/swyp/linkit/domain/user/service/UserService.java
+++ src/main/java/org/swyp/linkit/domain/user/service/UserService.java
@@ -1,12 +1,16 @@
 package org.swyp.linkit.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.swyp.linkit.domain.user.dto.request.NicknameUpdateRequestDto;
 import org.swyp.linkit.domain.user.entity.User;
 import org.swyp.linkit.domain.user.repository.UserRepository;
+import org.swyp.linkit.global.error.exception.DuplicateNicknameException;
 import org.swyp.linkit.global.error.exception.UserNotFoundException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -19,5 +23,23 @@ public class UserService {
                 .orElseThrow(() ->
                         new UserNotFoundException("존재하지 않는 사용자입니다")
                 );
+    }
+
+    // 닉네임 변경
+    @Transactional
+    public void updateNickname(Long userId, String nickname) {
+        // 1. 사용자 조회
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+
+        // 2. 닉네임 중복 검사
+        if (userRepository.existsByNickname(nickname)) {
+            throw new DuplicateNicknameException("이미 사용 중인 닉네임입니다.");
+        }
+
+        user.updateNickname(nickname);
+
+        log.info("닉네임 변경: userId={}, oldNickname={}, newNickname={}",
+                userId, user.getNickname(), nickname);
     }
 }
