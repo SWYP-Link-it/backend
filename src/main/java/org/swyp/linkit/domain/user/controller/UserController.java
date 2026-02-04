14f482e57776fb7a44743bfaf1fb4d46fb7eac8a
jaey-oung <jaeywodud7@gmail.com>
2026-02-05 04:05:37 +0900

    feat: 닉네임 변경 API 구현

---
+++ src/main/java/org/swyp/linkit/domain/user/controller/UserController.java
@@ -0,0 +1,53 @@
+package org.swyp.linkit.domain.user.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.swyp.linkit.domain.user.dto.request.NicknameUpdateRequestDto;
+import org.swyp.linkit.domain.user.service.UserService;
+import org.swyp.linkit.global.auth.oauth.CustomOAuth2User;
+import org.swyp.linkit.global.common.dto.ApiResponseDto;
+import org.swyp.linkit.global.swagger.annotation.ApiErrorExceptionsExample;
+import org.swyp.linkit.global.swagger.docs.UserExceptionDocs;
+
+@Tag(name = "User", description = "사용자 관련 API")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Operation(
+            summary = "닉네임 변경",
+            description = "로그인한 사용자의 닉네임을 변경합니다."
+    )
+    @ApiErrorExceptionsExample(UserExceptionDocs.UpdateNickname.class)
+    @PatchMapping(value = "/nickname", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponseDto<Void>> updateNickname(
+            @AuthenticationPrincipal CustomOAuth2User principal,
+            @Valid @RequestBody NicknameUpdateRequestDto requestDto) {
+
+        Long userId = principal.getUserId();
+
+        log.info("[UserProfile] PATCH /profile/nickname : userId={}, newNickname={}",
+                userId, requestDto.getNickname());
+
+        userService.updateNickname(userId, requestDto.getNickname());
+
+        return ResponseEntity.ok(
+                ApiResponseDto.success("닉네임이 변경되었습니다.", null)
+        );
+    }
+
+}
---
+++ src/main/java/org/swyp/linkit/global/swagger/docs/UserExceptionDocs.java
@@ -0,0 +1,45 @@
+package org.swyp.linkit.global.swagger.docs;
+
+import org.swyp.linkit.global.error.ErrorCode;
+import org.swyp.linkit.global.error.code.BaseErrorCode;
+import org.swyp.linkit.global.swagger.annotation.ExceptionDoc;
+import org.swyp.linkit.global.swagger.exception.SwaggerExampleExceptions;
+
+/**
+ * 사용자 도메인 API 예외 문서
+ */
+@ExceptionDoc
+public class UserExceptionDocs {
+    /**
+     * updateNickname
+     */
+    public static class UpdateNickname {
+        public static class UnauthorizedException implements SwaggerExampleExceptions {
+            @Override
+            public BaseErrorCode getErrorCode() {
+                return ErrorCode.UNAUTHORIZED;
+            }
+        }
+
+        public static class UserNotFoundException implements SwaggerExampleExceptions {
+            @Override
+            public BaseErrorCode getErrorCode() {
+                return ErrorCode.USER_NOT_FOUND;
+            }
+        }
+
+        public static class SameNicknameException implements SwaggerExampleExceptions {
+            @Override
+            public BaseErrorCode getErrorCode() {
+                return ErrorCode.SAME_NICKNAME;
+            }
+        }
+
+        public static class DuplicateNicknameException implements SwaggerExampleExceptions {
+            @Override
+            public BaseErrorCode getErrorCode() {
+                return ErrorCode.DUPLICATE_NICKNAME;
+            }
+        }
+    }
+}
