04cd945b3395ab20947beec986e3811dfbdc878d
홍정화 <ekdan31@gmail.com>
2026-02-12 01:01:48 +0900

    rename: 주석 수정

--- src/main/java/org/swyp/linkit/domain/review/service/UserRatingStatService.java
+++ src/main/java/org/swyp/linkit/domain/review/service/UserRatingStatService.java
@@ -7,7 +7,6 @@ public interface UserRatingStatService {
     /**
      *  유저 전체 평점 조회
      *
-     *  [재영님 필독]
      *  평점 정보 존재 시 소수점 한 자리까지 표현됩니다.
      *  평점 정보 존재 하지 않을 시 0.0 으로 처리됩니다.
      */
