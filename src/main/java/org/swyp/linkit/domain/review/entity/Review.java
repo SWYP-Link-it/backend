92330063cea3d2bc6dccb78e7a9c6deb8cd50da2
홍정화 <ekdan31@gmail.com>
2026-02-13 00:37:49 +0900

    feat: Review Entity 작성

---
+++ src/main/java/org/swyp/linkit/domain/review/entity/Review.java
@@ -0,0 +1,69 @@
+package org.swyp.linkit.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.swyp.linkit.global.common.domain.BaseTimeEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "skill_exchange_review_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long skillExchangeId;
+
+    @Column(nullable = false)
+    private Long revieweeSkillId;
+
+    @Column(nullable = false)
+    private Long reviewerId;
+
+    @Column(nullable = false)
+    private Long revieweeId;
+
+    @Column(length = 500, nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Review(Long skillExchangeId, Long revieweeSkillId, Long reviewerId,
+                   Long revieweeId, String content, Integer rating) {
+        this.skillExchangeId = skillExchangeId;
+        this.revieweeSkillId = revieweeSkillId;
+        this.reviewerId = reviewerId;
+        this.revieweeId = revieweeId;
+        this.content = content;
+        this.rating = rating;
+    }
+
+    // == 생성 메서드 ==
+    public static Review create(Long skillExchangeId, Long revieweeSkillId, Long reviewerId,
+                                Long revieweeId, String content, Integer rating) {
+        return Review.builder()
+                .skillExchangeId(skillExchangeId)
+                .revieweeSkillId(revieweeSkillId)
+                .reviewerId(reviewerId)
+                .revieweeId(revieweeId)
+                .content(content)
+                .rating(rating)
+                .build();
+    }
+
+    /**
+     *  비즈니스 메서드
+     */
+    public void update(String newContent, int newRating){
+        this.content = newContent;
+        this.rating = newRating;
+    }
+}
