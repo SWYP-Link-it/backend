8926cd1a60ee1020f9531ea3eeec6482dda2362d
rorrxr <rorrxr@naver.com>
2026-02-13 04:12:35 +0900

    feat: 채팅 이미지 업로드 API 구현

--- src/main/java/org/swyp/linkit/global/error/ErrorCode.java
+++ src/main/java/org/swyp/linkit/global/error/ErrorCode.java
@@ -201,7 +201,17 @@ public enum ErrorCode implements BaseErrorCode {
     NOTIFICATION_ALREADY_READ(HttpStatus.BAD_REQUEST, "N003", "이미 읽은 알림입니다."),
 
     @ExplainError("지원하지 않는 알림 타입을 요청한 경우 발생합니다.")
-    INVALID_NOTIFICATION_TYPE(HttpStatus.BAD_REQUEST, "N004", "유효하지 않은 알림 타입입니다.");
+    INVALID_NOTIFICATION_TYPE(HttpStatus.BAD_REQUEST, "N004", "유효하지 않은 알림 타입입니다."),
+
+    // 채팅 파일 업로드
+    @ExplainError("채팅 파일 크기가 10MB를 초과하는 경우 발생합니다.")
+    CHAT_FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "CF001", "파일 용량은 최대 10MB까지 업로드할 수 있습니다."),
+
+    @ExplainError("지원하지 않는 채팅 파일 형식인 경우 발생합니다.")
+    CHAT_FILE_TYPE_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "CF002", "지원하지 않는 파일 형식입니다. JPEG, PNG, GIF, WEBP 파일만 업로드할 수 있습니다."),
+
+    @ExplainError("채팅 파일 업로드 중 서버 오류가 발생한 경우입니다.")
+    CHAT_FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "CF003", "파일 업로드에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
