1663ec461c49dc556a81b750c5fbe239821883dc
jaey-oung <jaeywodud7@gmail.com>
2026-02-14 21:06:30 +0900

    feat: ReviewDetailQuery에 스킬 ID 필드 추가

--- src/main/java/org/swyp/linkit/domain/review/service/projection/ReviewDetailQuery.java
+++ src/main/java/org/swyp/linkit/domain/review/service/projection/ReviewDetailQuery.java
@@ -3,12 +3,12 @@ package org.swyp.linkit.domain.review.service.projection;
 import java.time.LocalDateTime;
 
 public record ReviewDetailQuery(
-    Long reviewId,
-    String reviewerNickname,
-    String skillName,
-    String content,
-    Integer rating,
-    LocalDateTime createdAt
+        Long reviewId,
+        String reviewerNickname,
+        String skillName,
+        Long skillId,
+        String content,
+        Integer rating,
+        LocalDateTime createdAt
 ){
 }
-
