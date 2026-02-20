07fe5f572c067112795ebbabeeed7c3bcc032504
홍정화 <ekdan31@gmail.com>
2026-02-20 22:51:08 +0900

    fix: 리뷰 등록, 수정 RequestDto 리뷰 내용 검증 적용

--- src/main/java/org/swyp/linkit/domain/review/dto/request/ReviewRequestDto.java
+++ src/main/java/org/swyp/linkit/domain/review/dto/request/ReviewRequestDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,6 +29,7 @@ public class ReviewRequestDto {
     private Long mentorId;
 
     @Schema(description = "리뷰 내용 (선택 사항)", example = "수업이 정말 유익했습니다.")
+    @Size(max = 500, message = "리뷰 내용은 최대 500자까지 입력 가능합니다.")
     private String content;
 
     @Schema(description = "평점 (1~5점)", example = "5")
