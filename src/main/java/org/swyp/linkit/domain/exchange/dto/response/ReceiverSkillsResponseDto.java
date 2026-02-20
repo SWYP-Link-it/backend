17e01bab65ddd0041d0543151436d795e857ba24
홍정화 <ekdan31@gmail.com>
2026-02-21 00:16:45 +0900

    refactor: 교환용 스킬 정보 응답 시 skillCategoryType 추가

--- src/main/java/org/swyp/linkit/domain/exchange/dto/response/ReceiverSkillsResponseDto.java
+++ src/main/java/org/swyp/linkit/domain/exchange/dto/response/ReceiverSkillsResponseDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.swyp.linkit.domain.user.dto.response.UserSkillForExchangeDto;
+import org.swyp.linkit.domain.user.entity.SkillCategoryType;
 
 @Getter
 @Builder
@@ -28,6 +29,9 @@ public class ReceiverSkillsResponseDto {
     @Schema(description = "크레딧 가격 (30분당 1크레딧)", example = "2")
     private Integer creditPrice;
 
+    @Schema(description = "스킬 카테고리 타입", example = "DEVELOPMENT")
+    private SkillCategoryType skillCategoryType;
+
     public static ReceiverSkillsResponseDto of(UserSkillForExchangeDto dto, String nickname) {
         return ReceiverSkillsResponseDto.builder()
                 .nickname(nickname)
@@ -35,6 +39,7 @@ public class ReceiverSkillsResponseDto {
                 .skillName(dto.getSkillName())
                 .exchangeDuration(dto.getExchangeDuration())
                 .creditPrice(dto.getCreditPrice())
+                .skillCategoryType(dto.getSkillCategoryType())
                 .build();
     }
 }
